26a1b81e345a21d1c047b39cef4f8ac948c1da91
Zo <ockleberryzalika@gmail.com>
2017-05-20 15:28:35 -0400

    font awesome

---
+++ distress-beacon/app/src/main/java/com/example/a502710129/distress_beacon/FontManager.java
@@ -0,0 +1,33 @@
+package com.example.a502710129.distress_beacon;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by 502710129 on 5/20/2017.
+ */
+
+public class FontManager {
+    public static final String ROOT = "fonts/",
+            FONTAWESOME = ROOT + "fontawesome-webfont.ttf";
+
+    public static Typeface getTypeface(Context context, String font) {
+        return Typeface.createFromAsset(context.getAssets(), font);
+    }
+    public static void markAsIconContainer(View v, Typeface typeface) {
+        if (v instanceof ViewGroup) {
+            ViewGroup vg = (ViewGroup) v;
+            for (int i = 0; i < vg.getChildCount(); i++) {
+                View child = vg.getChildAt(i);
+                markAsIconContainer(child);
+            }
+        } else if (v instanceof TextView) {
+            ((TextView) v).setTypeface(typeface);
+        }
+    }
+
+}
+}
+
